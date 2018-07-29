b88a2b0a8cf9a2250d24bab1991f8a391239eb19
Hailin_Ubuntu <hailinfufu@outlook.com>
2018-07-29 22:40:41 +0800

    二叉树的镜像

--- 剑指offer/树的子结构.cpp
+++ 剑指offer/树的子结构.cpp
@@ -26,12 +26,30 @@ TreeNode *CreateBTNode(int value)
     return (node);
 }
 
+bool DoesTree1HaveTree2(TreeNode *pRoot1, TreeNode *pRoot2)
+{
+    if (pRoot2 == NULL)
+        return true;
+    if (pRoot1 == NULL)
+        return false;
+    if (pRoot1->val != pRoot2->val)
+        return false;
+    return DoesTree1HaveTree2(pRoot1->left, pRoot2->left) && DoesTree1HaveTree2(pRoot1->right, pRoot2->right);
+}
+
 bool HasSubtree(TreeNode *pRoot1, TreeNode *pRoot2)
 {
-    if(pRoot2==nullptr)
-        return true;
-    if(pRoot1==nullptr)
-        return false;
+    bool result = false;
+    if (pRoot1 != NULL && pRoot2 != NULL)
+    {
+        if (pRoot1->val == pRoot2->val)
+            result = DoesTree1HaveTree2(pRoot1, pRoot2);
+        if (!result)
+            result = HasSubtree(pRoot1->left, pRoot2);
+        if (!result)
+            result = HasSubtree(pRoot1->right, pRoot2);
+    }
+    return result;
 }
 
 int main()
@@ -39,11 +57,11 @@ int main()
     // 树中结点含有分叉，树B是树A的子结构
     //                  8                8
     //              /       \           / \
-//             8         7         9   2
+    //             8         7         9   2
     //           /   \
-//          9     2
+    //          9     2
     //               / \
-//              4   7
+    //              4   7
 
     TreeNode *rootA = CreateBTNode(8);
 
