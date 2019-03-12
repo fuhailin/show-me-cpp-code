5e619bf6346f93f88871bf2023051e09dd8c6763
Hailin <hailinfufu@outlook.com>
2019-03-12 17:00:54 +0800

    数组的循环右移K位

---
+++ algorithm/move-elements-in-array-cyclically.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void print_vec(vector<int> &nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (i == nums.size() - 1)
+            cout << nums[i] << endl;
+        else
+            cout << nums[i] << " ";
+    }
+}
+
+void move(vector<int> &nums, int M)
+{
+    M = M % nums.size();
+    reverse(nums.begin(), nums.begin() + nums.size() - M);
+    // print_vec(nums);
+    reverse(nums.begin() + nums.size() - M, nums.end());
+    // print_vec(nums);
+    reverse(nums.begin(), nums.end());
+}
+
+int main(int argc, char const *argv[])
+{
+    int N, M;
+    cin >> N >> M;
+    vector<int> nums(N, 0);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> nums[i];
+    }
+    move(nums, M);
+    print_vec(nums);
+    return 0;
+}
