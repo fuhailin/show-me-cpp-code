3bbdf0c0b8ff074b9d2f597130c437f528f3e2f0
Hailin <hailinfufu@outlook.com>
2021-07-14 10:33:58 +0800

    最长回文子串，动态规划

--- cpp-tutorial/30_3rdlibrary/leetcode_cc/5.longest-palindromic-substring.cc
+++ cpp-tutorial/30_3rdlibrary/leetcode_cc/5.longest-palindromic-substring.cc
@@ -8,9 +8,38 @@ using namespace std;
 class Solution {
    public:
     string longestPalindrome(string s) {
-        int N = s.length();
-        vector<int> memo(N + 1, 0);
-        return s;
+        int n = s.length();
+        if (n < 2) {
+            return s;
+        }
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int maxLen = 1;
+        int begin = 0;
+        // 从短到长开始遍历
+        for (int L = 2; L <= n; L++) {
+            // 从左到右开始遍历
+            for (int i = 0; i < n; i++) {
+                int j = i + L - 1;
+                if (j >= n) {
+                    break;
+                }
+                if (s[i] == s[j]) {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+
+        return s.substr(begin, maxLen);
     }
 };
 
